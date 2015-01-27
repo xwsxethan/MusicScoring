3387f6c4b412663a385f4fde9337fd4548b97a6f
xwsxethan <xwsxethan@gmail.com>
2015-01-27 17:39:27 -0500

    Implemented the basic web page functionality, including the following:
    Navbar with links to the github page for info.
    Dropdown selector to choose between two pre-defined xmls to run.
    Output box to say which xml is currently selected.
    Button to execute the java code for the specified file.
    Output box to say what the score was once the code finishes.
    
    Closes xwsxethan/MusicScoring#30

--- WebCode/backend.php
+++ WebCode/backend.php
@@ -1,8 +1,10 @@
 <?php
-// Relative version should be ../MusicScoring/XMLtoScore/
-// chdir("C:/Users/user/Dropbox/Public/College/2015-01 Spring/Thesis/MusicScoring/XMLtoScore");
 
-// Relative jar is ../Executables/MusicScoring.jar
-// Relative xml is The_Hobbit_The_Desolation_of_Smaug_Medley_for_Solo_Clarinet.xml
-echo shell_exec('java -jar MusicScoring.jar "The_Hobbit_The_Desolation_of_Smaug_Medley_for_Solo_Clarinet.xml" 1');
+// chdir("C:/Users/user/Dropbox/Public/College/2015-01 Spring/Thesis/MusicScoring/XMLtoScore");
+$var = trim($_POST["xmlName"]);
+if (empty($var)) {
+	echo("No input file received. No score to post.");
+}
+
+echo shell_exec('java -jar MusicScoring.jar '.$var.' 1');
 ?>
