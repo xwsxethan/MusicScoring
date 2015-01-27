21aa082e81b4b808275fc37407b1915f05267da3
xwsxethan <xwsxethan@gmail.com>
2015-01-27 14:08:26 -0500

    This fixes the web code needed to execute some backend php when correctly placed in an apache directory. We can now run the new jar over mickey.cs.vt.edu and get results back to the front end.
    Since mickey is now setup and working, we have a baseline for the front
    end.
    
    Closes xwsxethan/MusicScoring#31

--- WebCode/backend.php
+++ WebCode/backend.php
@@ -1,8 +1,8 @@
 <?php
 // Relative version should be ../MusicScoring/XMLtoScore/
-chdir("C:/Users/user/Dropbox/Public/College/2015-01 Spring/Thesis/MusicScoring/XMLtoScore");
+// chdir("C:/Users/user/Dropbox/Public/College/2015-01 Spring/Thesis/MusicScoring/XMLtoScore");
 
 // Relative jar is ../Executables/MusicScoring.jar
 // Relative xml is The_Hobbit_The_Desolation_of_Smaug_Medley_for_Solo_Clarinet.xml
-echo exec('java -jar "C:/Users/user/Dropbox/Public/College/2015-01 Spring/Thesis/MusicScoring/Executables/MusicScoring.jar" "C:/Users/user/Dropbox/Public/College/2015-01 Spring/Thesis/MusicScoring/XMLtoScore/The_Hobbit_The_Desolation_of_Smaug_Medley_for_Solo_Clarinet.xml" 1');
+echo shell_exec('java -jar MusicScoring.jar "The_Hobbit_The_Desolation_of_Smaug_Medley_for_Solo_Clarinet.xml" 1');
 ?>
